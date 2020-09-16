a645460126d2629e57d6b5126e9f545358ee1b4b
Bhavna <bhavnawalia1234567@gmail.com>
2020-09-16 21:20:07 +0530

    commit

--- Day-11.py
+++ Day-11.py
@@ -1,15 +1,20 @@
-#!/bin/python3
+arr = []
+for arr_i in range(6):
+	arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+	arr.append(arr_t)
 
-import math
-import os
-import random
-import re
-import sys
+smax = -9 * 7
 
+for row in range(len(arr) - 2):
+	for column in range(len(arr[row]) - 2):
+		tl = arr[row][column]
+		tc = arr[row][column + 1]
+		tr = arr[row][column + 2]
+		mc = arr[row + 1][column + 1]
+		bl = arr[row + 2][column]
+		bc = arr[row + 2][column + 1]
+		br = arr[row + 2][column + 2]
+		s = tl + tc + tr + mc + bl + bc + br
+		smax = max(s, smax)
 
-
-if __name__ == '__main__':
-    arr = []
-
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
+print(smax)
